2c683df1bdbe590f632c3557a36100b14f721672
John Murianki <mcjthiongo@gmail.com>
2023-02-07 13:43:48 +0300

    fixed linter errors

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+/* eslint-disable no-plusplus */
 const hamburger = document.querySelector('.hamburger-icon');
 const Dropdownmenu = document.querySelector('.hidden-menu');
 const closeMenu = document.querySelector('.dropdown-x-icon');
 const hidemenu = document.getElementsByClassName('nav-dropdown-item-link');
-hamburger.addEventListener('click',function() {
-    Dropdownmenu.classList.add("show");});
-    const remove = function() {
-        Dropdownmenu.classList.remove("show");}
-    closeMenu.addEventListener('click',remove);
+hamburger.addEventListener('click', () => {
+  Dropdownmenu.classList.add('show');
+});
+const remove = function r() {
+  Dropdownmenu.classList.remove('show');
+};
+closeMenu.addEventListener('click', remove);
 for (let i = 0; i < hidemenu.length; i++) {
-    hidemenu[i].addEventListener('click', remove);
-  }
+  hidemenu[i].addEventListener('click', remove);
+}
